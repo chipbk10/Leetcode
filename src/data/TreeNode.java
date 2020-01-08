1869e35ae71cbd606ce3c9e8a51dad2e171b7bff
Hieu Luong <hieu.luong.dinh.ext@proximus.com>
2020-01-09 00:44:37 +0100

    Solve problem 95 - unique binary search tree II

---
+++ src/data/TreeNode.java
@@ -0,0 +1,10 @@
+package data;
+
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left, right;
+    public TreeNode(int x) { val = x; }
+}
