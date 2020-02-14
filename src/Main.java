a02212f649a496ebae94b2683eebc4f9b5fce95f
Hieu Luong <hieu.luong.dinh.ext@proximus.com>
2020-02-14 15:17:13 +0100

    Solve Problem 221 && 871

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import backtrack.Problem1240_TilingRectangleWithTheFewestSquares;
+import dp.min_max_path_to_reach_target.Problem871_MinimumNumberOfRefuelingStops;
 
 public class Main {
 
     public static void main(String[] args) {
-        Problem1240_TilingRectangleWithTheFewestSquares.run();
+        Problem871_MinimumNumberOfRefuelingStops.run();
     }
 }
