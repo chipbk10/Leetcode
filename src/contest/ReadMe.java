7fd03a04d40e5f43473cf9f30eab310e55b40be9
hieu <hieu.luongdinh@bnpparibasfortis.com>
2020-03-23 09:00:23 +0100

    Solve Problem 864 - Shortest Path To Get All Keys

--- src/contest/ReadMe.java
+++ src/contest/ReadMe.java
@@ -18,9 +18,9 @@ public class ReadMe {
     // sliding window:      1208[v]     1040[!]     995[!]
     // linked-list:         1290[v]     1019[!]     876[v]
     // union-find:          1202[v]     924[v]      947[!]
-    // heap:                1054[!]     882[!]      864
-    // design:              1352[x]     1348[x]     705
-    // bit manipulation:    1356[v]     1342[v]     1318
+    // heap:                1054[!]     882[!]      864[x]
+    // design:              1352[x]     1348[x]     705[v]
+    // bit manipulation:    1356[v]     1342[v]     1318[v]
     // other:               913[x]      1288[v]     843
 
 
