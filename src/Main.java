ffd8792d2baf040fae29f9a1864947f618585de0
hieu <hieu.luongdinh@bnpparibasfortis.com>
2020-04-24 04:24:27 +0200

    Solve Problem - BitWise AND of numbers range

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import slidingwindow.Problem992_SubarraysWithKDifferentIntegers;
+import bit.Problem_BitWiseANDofNumbersRange;
 
 public class Main {
 
     public static void main(String[] args) {
-        Problem992_SubarraysWithKDifferentIntegers.run();
+        Problem_BitWiseANDofNumbersRange.run();
     }
 }
