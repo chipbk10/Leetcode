5769ce9aa343dab663a99700148c252c1aa27f05
Hieu Luong <hieu.luong.dinh.ext@proximus.com>
2020-07-15 16:38:29 +0200

    Update Problem 1358

--- src/slidingwindow/Problem1358_NumberOfSubstringsContainingAllThreeCharacters.java
+++ src/slidingwindow/Problem1358_NumberOfSubstringsContainingAllThreeCharacters.java
@@ -4,10 +4,10 @@ public class Problem1358_NumberOfSubstringsContainingAllThreeCharacters {
 
     public int numberOfSubstrings(String s) {
         // abcabc
-        // abc: 0-x-0 = (0+1) * (0+1) = 1
-        // abca: 1-x-0 = (1+1) * (0+1) = 2
-        // abcab: 2-x-0 = (2+1) * (0+1) = 3
-        // abcabc: 3-x-0 = (3+1) * (0+1) = 4
+        // abc: 0-x = (0+1) = 1
+        // abca: 1-x = (1+1) = 2
+        // abcab: 2-x = (2+1) = 3
+        // abcabc: 3-x = (3+1) = 4
         int na = 0, nb = 0, nc = 0, l = 0, res = 0;
         for (int i = 0, j = 0; i < s.length(); i++) {
             char c = s.charAt(i);
